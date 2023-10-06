267d303f9831e7c54dda1844ee4ea0d61c3b9548
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-10-06 08:22:08 +0300

    test

--- js/config.js
+++ js/config.js
@@ -8,4 +8,4 @@ window._config = {
     invokeUrl: "https://x6pf70psha.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 }
-//test
+//test 1
